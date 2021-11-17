8a04cc66bcfba0c0e64a71bcd8a8a3b0d8b197d0
PaulJD88 <77843480+PaulJD88@users.noreply.github.com>
2021-11-17 21:36:47 +0000

    First Additions

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
+    <button @click="getBuilds"> Get Builds </button>
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    getBuilds () {
+      console.log('Getting Builds!')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
