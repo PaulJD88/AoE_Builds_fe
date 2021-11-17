3d7a23e8f3e29da95ad8696d1ee8a79dd827efc7
PaulJD88 <77843480+PaulJD88@users.noreply.github.com>
2021-11-17 21:57:56 +0000

    API integration

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <button @click="getBuilds"> Get Builds </button>
+    <button @click="getBuilds">Get Builds</button>
     <h1>This is an about page</h1>
   </div>
 </template>
@@ -10,6 +10,9 @@ export default {
   methods: {
     getBuilds () {
       console.log('Getting Builds!')
+      fetch('http://localhost:3000/builds')
+        .then((response) => response.json())
+        .then((data) => console.log(data))
     }
   }
 }
