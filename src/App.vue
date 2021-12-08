85d9232127a80f51381ce602f51ce8536594cc68
PaulJD88 <77843480+PaulJD88@users.noreply.github.com>
2021-12-08 21:48:06 +0000

    Index.js Changes

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,13 @@
     <section>
       <nav>
         <ul>
-          <button><router-link to="/cover">Home</router-link></button>
           <button><router-link to="/create">Create Build</router-link></button>
+          <button><router-link to="/">Home</router-link></button>
         </ul>
       </nav>
 
       <article>
-        <h1>Welcome</h1>
-        <p>
-          This is a repository for Age of Empires IV players to find the highest
-          rated builds for the civilisations found in the game.
-        </p>
-        <p>
-          As well as find links to rated builds you can also create your own
-          build through the provided template.
-        </p>
+        <router-view />
         <buildslct></buildslct>
       </article>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<article>
+  <h1>Welcome</h1>
+  <p>
+    This is a repository for Age of Empires IV players to find the highest rated
+    builds for the civilisations found in the game.
+  </p>
+  <p>
+    As well as find links to rated builds you can also create your own build
+    through the provided template.
+  </p>
+</article>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
